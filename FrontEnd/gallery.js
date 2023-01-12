360f31bed7954ccf7350d1f6500a94b72953bedc
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-01-12 18:14:32 +0100

    testative de JS

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,13 +1,15 @@
-[
-    
-    
-    {
-    "id": 1,
-    "nom": "Ampoule LED",
-    "prix": 60,
-    "categorie": "Optiques",
-    "image": "images/abajour-tahina.png/",
-    "description": "Distance d’éclairage : 100 mètres !",
-    "disponibilite": true
-}
-]
+const reponse = await fetch("gallery.json");
+const pieces = await reponse.json();
+
+const article = pieces[0];
+
+const sectionGallery = document.querySelector(".gallery");
+
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+
+const nomElement = document.createElement("figcaption");
+nomElement.innerText = article.nom;
+
+sectionGallery.appendChild(imageElement);
+sectionGallery.appendChild(nomElement);

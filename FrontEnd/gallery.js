aad7f80510077362cd7700859ac509afdd3d1934
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-01-12 15:42:17 +0100

    GO P3

---
+++ FrontEnd/gallery.js
@@ -0,0 +1,13 @@
+[
+    
+    
+    {
+    "id": 1,
+    "nom": "Ampoule LED",
+    "prix": 60,
+    "categorie": "Optiques",
+    "image": "images/abajour-tahina.png/",
+    "description": "Distance d’éclairage : 100 mètres !",
+    "disponibilite": true
+}
+]

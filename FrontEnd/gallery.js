c35acdef82786b0a7f22c9859bf4f26a3824424b
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-01-13 12:15:53 +0100

    filtres+gallery

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,15 +1,30 @@
-const reponse = await fetch("gallery.json");
-const pieces = await reponse.json();
+// Récupération des cartes depuis le fichier JSON
+const reponse = await fetch('gallery.json');
+const gallery = await reponse.json();
 
-const article = pieces[0];
-
-const sectionGallery = document.querySelector(".gallery");
+for (let i = 0; i < gallery.length; i++) {
+// Elements récupérés
+const article = gallery[i];
 
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 
-const nomElement = document.createElement("figcaption");
+const nomElement = document.createElement("h3");
 nomElement.innerText = article.nom;
 
+const sectionGallery = document.querySelector(".gallery");
 sectionGallery.appendChild(imageElement);
 sectionGallery.appendChild(nomElement);
+
+};
+
+// Filtres
+
+const boutonObjet = document.querySelector(".btnobjet");
+
+boutonObjet.addEventListener("click", function () {
+    const galleryObjet = gallery.filter(function (gallery) {
+        return gallery.categorie["Objet"] /*objet.....????*/
+    });
+   console.log(galleryObjet)
+});

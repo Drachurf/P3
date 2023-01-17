f74a486e5f317252d58b2c53910b951bbb2087dc
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-01-17 17:46:41 +0100

    galery ok depuis API

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,52 +1,63 @@
-// Récupération des cartes depuis le fichier JSON
-const reponse = await fetch('gallery.json');
-const articles = await reponse.json();
+// Récupération des cartes depuis API
+const works = await fetch('http://localhost:5678/api/works');
+const articles = await works.json();
+const categories = await fetch ('http://localhost:5678/api/categories');
+const filtres = await categories.json();
+
 
 function genererArticles(articles){
-for (let i = 0; i < articles.length; i++) {
-// Elements récupérés
+    for (let i = 0; i < articles.length; i++) {
+
         const article = articles[i];
+    //      Récupération de l'élément du DOM qui accueillera les fiches + créations balises
         const sectionGallery = document.querySelector(".gallery");
-
         const galleryElement = document.createElement("article");
-
         const imageElement = document.createElement("img");
-        imageElement.src = article.image;
+           imageElement.src = article.imageUrl;
+           imageElement.crossOrigin = 'anonymous';
+           imageElement.alt = article.title;
+
         const nomElement = document.createElement("h3");
-        nomElement.innerText = article.nom;
+            nomElement.innerText = article.title;
+
 
         sectionGallery.appendChild(galleryElement);
-
         galleryElement.appendChild(imageElement);
         galleryElement.appendChild(nomElement);
 
 }}
-genererArticles(articles);
+genererArticles(articles)
 
-// Filtres
+
+// Filtres Objet id: 1
+
 const boutonObjet = document.querySelector(".btnobjet");
+
 boutonObjet.addEventListener("click", function () {
-    
-    const articlesObjet = articles.filter(function (article){
-        return article.categorie;
+    const filtreObjet = filtres.filter(function (article){
+        return filtres.src = 10
     });
     document.querySelector(".gallery").innerHTML = "";
-    genererArticles(articlesObjet);
-
-    console.log(articlesObjet);
+    console.log(filtreObjet);
+    genererArticles();
 });
 
-const boutonTous = document.querySelector(".btntous");
 
+/*boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+    document.querySelector(".fiches").innerHTML = "";
+    genererPieces(piecesFiltrees);
+});*/
+
+// Filtres Tous
+const boutonTous = document.querySelector(".btntous");
 boutonTous.addEventListener("click", function () {
 
     const articlesNofiltre = articles.filter(function (article){
-        return article.id
+        return article.id = 1
     });
     document.querySelector(".gallery").innerHTML = "";
     genererArticles(articlesNofiltre);
-
-    console.log(articlesNofiltre);
 });
-
-

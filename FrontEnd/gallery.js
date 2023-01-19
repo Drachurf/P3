a7b3b90cf9ebf540fa91e68f8084c9d9368e3364
Drachurf <121042040+Drachurf@users.noreply.github.com>
2023-01-19 10:39:13 +0100

    btn-filtres-ok

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,63 +1,109 @@
 // Récupération des cartes depuis API
-const works = await fetch('http://localhost:5678/api/works');
+const works = await fetch("http://localhost:5678/api/works");
 const articles = await works.json();
-const categories = await fetch ('http://localhost:5678/api/categories');
+const categories = await fetch("http://localhost:5678/api/categories");
 const filtres = await categories.json();
 
+function genererArticles(articles) {
+  for (let i = 0; i < articles.length; i++) {
+    const article = articles[i];
+    //Récupération de l'élément du DOM qui accueillera les fiches + créations balises
+    const sectionGallery = document.querySelector(".gallery");
+    const galleryElement = document.createElement("article");
+    const imageElement = document.createElement("img");
+    imageElement.src = article.imageUrl;
+    imageElement.crossOrigin = "anonymous";
+    imageElement.alt = article.title;
 
-function genererArticles(articles){
-    for (let i = 0; i < articles.length; i++) {
+    const nomElement = document.createElement("h3");
+    nomElement.innerText = article.title;
 
-        const article = articles[i];
-    //      Récupération de l'élément du DOM qui accueillera les fiches + créations balises
-        const sectionGallery = document.querySelector(".gallery");
-        const galleryElement = document.createElement("article");
-        const imageElement = document.createElement("img");
-           imageElement.src = article.imageUrl;
-           imageElement.crossOrigin = 'anonymous';
-           imageElement.alt = article.title;
+    sectionGallery.appendChild(galleryElement);
+    galleryElement.appendChild(imageElement);
+    galleryElement.appendChild(nomElement);
+  }
+}
+genererArticles(articles);
 
-        const nomElement = document.createElement("h3");
-            nomElement.innerText = article.title;
+// filtre TOUS
+const boutonTous = document.querySelector("#btntous");
+boutonTous.addEventListener("click", function () {
+  const articlesNofiltre = articles.filter(function (article) {
+    return article.id
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  console.log(articlesNofiltre);
+  genererArticles(articlesNofiltre);
+});
 
-
-        sectionGallery.appendChild(galleryElement);
-        galleryElement.appendChild(imageElement);
-        galleryElement.appendChild(nomElement);
-
-}}
-genererArticles(articles)
-
-
-// Filtres Objet id: 1
-
-const boutonObjet = document.querySelector(".btnobjet");
+// Filtres Objet id: 1 = objet
+const boutonObjet = document.querySelector("#btnobjet");
 
 boutonObjet.addEventListener("click", function () {
-    const filtreObjet = filtres.filter(function (article){
-        return filtres.src = 10
-    });
-    document.querySelector(".gallery").innerHTML = "";
-    console.log(filtreObjet);
-    genererArticles();
+  const filtreObjets = articles.filter(function (article) {
+    return article.category.id === 1;
+  });
+
+  document.querySelector(".gallery").innerHTML = "";
+  console.log(filtreObjets);
+  genererArticles(filtreObjets);
 });
 
+// Filtres Appartement id: 2
+const boutonAppart = document.querySelector("#btnappart");
 
-/*boutonFiltrer.addEventListener("click", function () {
-    const piecesFiltrees = pieces.filter(function (piece) {
-        return piece.prix <= 35;
-    });
-    document.querySelector(".fiches").innerHTML = "";
-    genererPieces(piecesFiltrees);
-});*/
+boutonAppart.addEventListener("click", function () {
+  const filtreApparts = articles.filter(function (article) {
+    return article.category.id === 2;
+  });
 
-// Filtres Tous
-const boutonTous = document.querySelector(".btntous");
-boutonTous.addEventListener("click", function () {
-
-    const articlesNofiltre = articles.filter(function (article){
-        return article.id = 1
-    });
-    document.querySelector(".gallery").innerHTML = "";
-    genererArticles(articlesNofiltre);
+  document.querySelector(".gallery").innerHTML = "";
+  console.log(filtreApparts);
+  genererArticles(filtreApparts);
 });
+
+// Filtres Hôtel id: 3
+const boutonHotel = document.querySelector("#btnhotel");
+
+boutonHotel.addEventListener("click", function () {
+  const filtreHotels = articles.filter(function (article) {
+    return article.category.id === 3;
+  });
+
+  document.querySelector(".gallery").innerHTML = "";
+  console.log(filtreHotels);
+  genererArticles(filtreHotels);
+});
+
+const boutonSubmit = document.querySelector("#submit");
+boutonSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const email = document.querySelector("#email");
+  const password = document.querySelector("#password");
+
+  let login = {
+    email: email,
+    password: password,
+  };
+
+  let response = fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(login),
+  });
+
+  console.log(response);
+});
+
+/* function(json) { // Si ça c'est passé avec succès
+    // ici on teste la réponse
+    if(json.reponse === 'ok') {
+      alert('Connexion OK');
+      // On peut aussi rediriger vers l'index
+      window.location.href = 'index.html';
+    } else {
+      alert('Erreur : '+ json.reponse);  }
+    };*/
